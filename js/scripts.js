6e82900bc7d536d4c8d818926597e33821d8c0ff
airada <jasmineirada@gmail.com>
2020-09-05 16:52:45 -0700

    made changes to chatbox and chat area sizing

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 let user_reply = "";
+let chat_box =  $('#chat_box');
 
 function onSubmit(chatbox){
     chatbox.preventDefault();
     user_reply = $("#send-on-enter").val();
     console.log("User replied: " + user_reply);
-    $('#chatbox')[0].reset();
+    chat_box[0].reset();
 }
 
-$("#chatbox").submit(onSubmit);
+chat_box.submit(onSubmit);
