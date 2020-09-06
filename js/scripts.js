3cafd48d559f2c64f16d6d910fab29a8814222bb
airada <jasmineirada@gmail.com>
2020-09-05 18:08:56 -0700

    added styling to chat messages and a dummy chat reply

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 let user_reply = "";
 let chat_box =  $('#chat_box');
 
+function bot_reply(input){
+    let chat_area = jQuery("#chat_area");
+
+    let rowHTML = "";
+
+    rowHTML += "<div class='row ml-1 py-2'> <div class='col bg-light text-left'><p class='w-25 h-100 rounded text-white bg-warning'>yay!</p></div></div>";
+    chat_area.append(rowHTML);
+}
+
 function add_message(input){
     let chat_area = jQuery("#chat_area");
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row bg-primary ml-1 py-2'> <div class='col bg-warning'><p class='w-100'>" + input + "</p></div></div>";
+    rowHTML += "<div class='row ml-1 py-2'> <div class='col d-flex justify-content-end text-right'><p class='w-25 h-100 rounded text-white bg-primary'>" + input + "</p></div></div>";
     chat_area.append(rowHTML);
+
+    bot_reply(input);
+    var objDiv = document.getElementById("chat");
+    objDiv.scrollTop = objDiv.scrollHeight;
 
 
 }
