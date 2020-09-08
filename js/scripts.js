1b050d0e6300f9cbedf4718b2293baf40836017d
airada <jasmineirada@gmail.com>
2020-09-07 17:11:29 -0700

    fixed UI for chat bubbles and responsiveness for tablet and mobile devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let user_reply = "";
+let user_input = "";
 let chat_box =  $('#chat_box');
 
 function bot_reply(input){
@@ -6,31 +6,36 @@ function bot_reply(input){
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row ml-1 py-2'> <div class='col bg-light text-left'><p class='w-25 h-100 rounded text-white bg-warning'>yay!</p></div></div>";
+    rowHTML += "<div class='row my-2'>" +
+    "<div class='col-12 pl-0 d-flex align-items-center justify-content-start text-center'>" +
+    "<div class='px-2'><img src='./img/chatbot.png' alt'...' class='rounded-circle'></div>"+
+    "<p class='p-2 my-auto mw-custom rounded text-white bg-chatbot'>" + input + "</p>" +
+    "</div></div>";
     chat_area.append(rowHTML);
 }
 
-function add_message(input){
+function user_reply(input){
     let chat_area = jQuery("#chat_area");
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row ml-1 py-2'> <div class='col d-flex justify-content-end text-right'><p class='w-25 h-100 rounded text-white bg-primary'>" + input + "</p></div></div>";
+    rowHTML += "<div class='row my-2'>" +
+        "<div class='col-12 pr-0 d-flex align-items-center justify-content-end text-center'>" +
+        "<p class='p-2 my-auto mw-custom rounded text-white bg-user'>" + input + "</p>" +
+        "<div class='px-2'><img src='./img/user.png' alt'...' class='rounded-circle'></div></div></div>";
     chat_area.append(rowHTML);
 
-    bot_reply(input);
+    bot_reply("yay!");
     var objDiv = document.getElementById("chat");
     objDiv.scrollTop = objDiv.scrollHeight;
-
-
 }
 
 function onSubmit(chatbox){
     chatbox.preventDefault();
-    user_reply = $("#send-on-enter").val();
-    console.log("User replied: " + user_reply);
+    user_input = $("#send-on-enter").val();
     chat_box[0].reset();
-    add_message(user_reply);
+    user_reply(user_input);
 }
 
+bot_reply("Hello! What's up?");
 chat_box.submit(onSubmit);
