d083fe855f5669518bc2e2b35546370ac37fe19c
airada <jasmineirada@gmail.com>
2020-09-11 22:03:59 -0700

    changed css

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function bot_reply(input){
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row my-2'>" +
+    rowHTML += "<div class='row m-2'>" +
     "<div class='col-12 pl-0 d-flex align-items-center justify-content-start text-center'>" +
     "<div class='px-2'><img src='./img/chatbot.png' alt'...' class='rounded-circle'></div>"+
     "<p class='p-2 my-auto mw-custom rounded text-white bg-chatbot'>" + input + "</p>" +
@@ -19,7 +19,7 @@ function user_reply(input){
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row my-2'>" +
+    rowHTML += "<div class='row m-2'>" +
         "<div class='col-12 pr-0 d-flex align-items-center justify-content-end text-center'>" +
         "<p class='p-2 my-auto mw-custom rounded text-white bg-user'>" + input + "</p>" +
         "<div class='px-2'><img src='./img/user.png' alt'...' class='rounded-circle'></div></div></div>";
