02f50605868a91fbf3d8c7b92240cfcaa26693ff
airada <jasmineirada@gmail.com>
2020-09-14 19:04:25 -0700

    added dflex and set messages to the bottom of chat area

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 let user_input = "";
 let chat_box =  $('#chat_box');
 
-function bot_reply(input){
+function bot_reply(input, first_msg = false){
     let chat_area = jQuery("#chat_area");
+
+    
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row m-2'>" +
-    "<div class='col-12 pl-0 d-flex align-items-center justify-content-start text-center'>" +
+    if (first_msg){
+        rowHTML += "<div class='row mx-2 mb-2 mt-auto'>";
+    } else {
+        rowHTML += "<div class='row m-2'>";
+    }
+    rowHTML += "<div class='col-12 pl-0 d-flex align-items-center justify-content-start text-center'>" +
     "<div class='px-2'><img src='./img/chatbot.png' alt'...' class='rounded-circle'></div>"+
     "<p class='p-2 my-auto mw-custom rounded text-white bg-chatbot'>" + input + "</p>" +
     "</div></div>";
@@ -26,7 +32,7 @@ function user_reply(input){
     chat_area.append(rowHTML);
 
     bot_reply("yay!");
-    var objDiv = document.getElementById("chat");
+    var objDiv = document.getElementById("chat_area");
     objDiv.scrollTop = objDiv.scrollHeight;
 }
 
@@ -48,7 +54,7 @@ function first_row() {
 
 // first_row();
 
-bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?", true);
 bot_reply("Hello! What's up?");
 
 
