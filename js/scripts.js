b9cfd8dfc646639ac9a91329190bd83842e0036e
airada <jasmineirada@gmail.com>
2020-09-14 18:05:36 -0700

    scroll broke again; taking out textbox

--- js/scripts.js
+++ js/scripts.js
@@ -48,5 +48,27 @@ function first_row() {
 
 // first_row();
 bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+
 
 chat_box.submit(onSubmit);
