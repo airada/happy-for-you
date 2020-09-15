1e0c3cc6c8b1720df2c6da05c317a5869aa8b948
airada <jasmineirada@gmail.com>
2020-09-14 17:22:33 -0700

    testing scroll

--- js/scripts.js
+++ js/scripts.js
@@ -48,4 +48,19 @@ function first_row() {
 
 // first_row();
 bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+bot_reply("Hello! What's up?");
+
 chat_box.submit(onSubmit);
