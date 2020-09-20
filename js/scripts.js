4ae51f757485957d9a9d05f25d835e02a121773e
airada <jasmineirada@gmail.com>
2020-09-20 11:19:19 -0700

    row collapsing issue resolved, removed the bg colors

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ function bot_reply(input, first_msg = false){
     let rowHTML = "";
 
     if (first_msg){
-        rowHTML += "<div class='row p-1 my-1 mx-2 pr-auto mr-1 bg-secondary msg-height d-flex align-items-center justify-content-start text-center'>";
+        rowHTML += "<div class='row p-1 mt-auto mb-1 mx-2 pr-auto mr-1 msg-height d-flex align-items-center justify-content-start text-center'>";
     } else {
-        rowHTML += "<div class='row msg-height p-1 my-1 mx-2 mr-1 bg-secondary d-flex align-items-center justify-content-start text-center'>";
+        rowHTML += "<div class='row msg-height p-1 my-1 mx-2 mr-1 d-flex align-items-center justify-content-start text-center'>";
     }
     rowHTML += 
-    "<img src='./img/chatbot.png' alt'...' class='p-2 m-1 bg-warning'>"+
-    "<p class='my-auto mx-1 p-2 bg-danger size-custom rounded text-white bg-chatbot'>" + input + "</p></div>";
+    "<img src='./img/chatbot.png' alt'...' class='p-2 m-1'>"+
+    "<p class='my-auto mx-1 p-2 size-custom rounded text-white bg-chatbot'>" + input + "</p></div>";
     chat_area.append(rowHTML);
 }
 
@@ -22,9 +22,9 @@ function user_reply(input){
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row msg-height p-1 my-1 mx-2 pl-auto bg-secondary d-flex align-items-center justify-content-end text-center'>" +
+    rowHTML += "<div class='row msg-height p-1 my-1 mx-2 pl-auto d-flex align-items-center justify-content-end text-center'>" +
         "<p class='mt-2 mb-1 p-2 ml-auto size-custom rounded text-white bg-user'>" + input + "</p>" +
-        "<img src='./img/user.png' alt'...' class='p-2 m-1 bg-warning'></div>";
+        "<img src='./img/user.png' alt'...' class='p-2 m-1'></div>";
 
     chat_area.append(rowHTML);
 
