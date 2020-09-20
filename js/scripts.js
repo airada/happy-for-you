60fb67fab55fda9a97c2f90984dd0452492ad351
airada <jasmineirada@gmail.com>
2020-09-20 11:00:44 -0700

    added msg height

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@ function bot_reply(input, first_msg = false){
     let rowHTML = "";
 
     if (first_msg){
-        rowHTML += "<div class='p-1 my-1 mx-2 pr-auto mr-1 bg-secondary'>";
+        rowHTML += "<div class='row p-1 my-1 mx-2 pr-auto mr-1 bg-secondary msg-height d-flex align-items-center justify-content-start text-center'>";
     } else {
-        rowHTML += "<div class='p-1 my-1 mx-2 mr-1 bg-secondary d-flex align-items-center justify-content-start text-center'>";
+        rowHTML += "<div class='row msg-height p-1 my-1 mx-2 mr-1 bg-secondary d-flex align-items-center justify-content-start text-center'>";
     }
     rowHTML += 
     // "<img src='./img/chatbot.png' alt'...' class='p-2 m-1 bg-warning'>"+
@@ -22,7 +22,7 @@ function user_reply(input){
 
     let rowHTML = "";
 
-    rowHTML += "<div class='p-1 my-1 mx-2 pl-auto bg-secondary d-flex align-items-center justify-content-end text-center'>" +
+    rowHTML += "<div class='row msg-height p-1 my-1 mx-2 pl-auto bg-secondary d-flex align-items-center justify-content-end text-center'>" +
         "<p class='mt-2 mb-1 p-2 ml-auto size-custom rounded text-white bg-user'>" + input + "</p>" +
         // "<img src='./img/user.png' alt'...' class='p-2 m-1 bg-warning'>" 
         "</div>";
