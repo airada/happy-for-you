673cfecea66c4b6a8499956031e0bc3dab321da0
airada <jasmineirada@gmail.com>
2020-09-20 10:52:41 -0700

    removed row class

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ function bot_reply(input, first_msg = false){
     let rowHTML = "";
 
     if (first_msg){
-        rowHTML += "<div class='row p-1 my-1 mx-2 pr-auto mr-1 bg-secondary'>";
+        rowHTML += "<div class='p-1 my-1 mx-2 pr-auto mr-1 bg-secondary'>";
     } else {
-        rowHTML += "<div class='row p-1 my-1 mx-2 mr-1 bg-secondary d-flex align-items-center justify-content-start text-center'>";
+        rowHTML += "<div class='p-1 my-1 mx-2 mr-1 bg-secondary d-flex align-items-center justify-content-start text-center'>";
     }
     rowHTML += 
     // "<img src='./img/chatbot.png' alt'...' class='p-2 m-1 bg-warning'>"+
-    "<p class='my-auto mx-1 p-2 bg-danger size-custom rounded text-white bg-chatbot msg-position'>" + input + "</p></div>";
+    "<p class='my-auto mx-1 p-2 bg-danger size-custom rounded text-white bg-chatbot'>" + input + "</p></div>";
     chat_area.append(rowHTML);
 }
 
@@ -22,8 +22,8 @@ function user_reply(input){
 
     let rowHTML = "";
 
-    rowHTML += "<div class='row p-1 my-1 mx-2 pl-auto bg-secondary d-flex align-items-center justify-content-end text-center'>" +
-        "<p class='mt-2 mb-1 p-2 ml-auto size-custom rounded text-white bg-user msg-position'>" + input + "</p>" +
+    rowHTML += "<div class='p-1 my-1 mx-2 pl-auto bg-secondary d-flex align-items-center justify-content-end text-center'>" +
+        "<p class='mt-2 mb-1 p-2 ml-auto size-custom rounded text-white bg-user'>" + input + "</p>" +
         // "<img src='./img/user.png' alt'...' class='p-2 m-1 bg-warning'>" 
         "</div>";
 
